8fcca424292808e7e5051c9be58c5a28d3a1b153
DylanTaylor1 <89085119+DylanTaylor1@users.noreply.github.com>
2023-05-23 15:04:09 -0400

    Max Stat Flexibility
    Maximum stats can be individually changed for each program

--- autoSpread.lua
+++ autoSpread.lua
@@ -24,13 +24,22 @@ end
 
 -- ====================== SCANNING ======================
 
+local function isWeed(crop)
+    return crop.name == 'weed' or
+        crop.name == 'Grass' or
+        crop.gr > config.autoSpreadMaxGrowth or
+        crop.re > config.autoSpreadMaxResistance or
+        (crop.name == 'venomilia' and crop.gr > 7)
+end
+
+
 local function checkChild(slot, crop)
     if crop.isCrop and crop.name ~= 'emptyCrop' then
 
         if crop.name == 'air' then
             action.placeCropStick(2)
 
-        elseif scanner.isWeed(crop) then
+        elseif isWeed(crop) then
             action.deweed()
             action.placeCropStick()
 
@@ -72,7 +81,7 @@ end
 
 local function checkParent(slot, crop)
     if crop.isCrop and crop.name ~= 'air' and crop.name ~= 'emptyCrop' then
-        if scanner.isWeed(crop) then
+        if isWeed(crop) then
             action.deweed()
             database.updateFarm(slot, {isCrop=true, name='emptyCrop'})
         end
